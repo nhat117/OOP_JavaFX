722c2925ad086aec441bdca5f40d27cedbffb561
tribuiminh <uy>
2021-08-26 17:45:17 +0700

    new commitcommit

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,19 @@
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        calc();
+    }
 
     @FXML
     Circle cir1;
@@ -13,9 +23,42 @@ public class Controller {
 
     @FXML
     Line line;
-    @FXML
-    public void cirMove(MouseEvent mouseEvent) {
-        System.out.println("Circle is move");
 
+    @FXML
+    Label lab;
+
+
+    @FXML
+    protected void  cir1Move(MouseEvent mouse) {
+        cir1.setCenterX(mouse.getX());
+        cir1.setCenterY(mouse.getY());
+        line.setStartX(mouse.getX());
+        line.setStartY(mouse.getY());
+        calc();
+        moveLab();
+    }
+
+    @FXML
+    protected void cir2Move(MouseEvent mouse) {
+        cir2.setCenterX(mouse.getX());
+        cir2.setCenterY(mouse.getY());
+        line.setEndX(mouse.getX());
+        line.setEndY(mouse.getY());
+        calc();
+        moveLab();
+    }
+
+    private void calc() {
+        double distance = Math.sqrt(Math.pow(cir1.getCenterX() - cir2.getCenterX(),2)
+                + Math.pow(cir1.getCenterY() - cir2.getCenterY(),2));
+        lab.setText(Integer.toString((int)distance));
+    }
+
+    private void moveLab() {
+        double midX = (cir1.getCenterX() + cir2.getCenterX()) /2;
+        double midY=(cir1.getCenterY() + cir2.getCenterY()) /2;
+        lab.setLayoutX(midX);
+        lab.setLayoutY(midY);
     }
 }
+
